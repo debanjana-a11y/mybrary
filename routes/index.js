b86e66cf39d57fa088daee9e80f8c73360ac9c0d
debanjana-a11y <89571066+debanjana-a11y@users.noreply.github.com>
2024-05-21 22:34:04 +0530

    Adds more CSS in homepage

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,17 @@ const express = require('express');
 const route = express.Router();
 
 route.get('/', (req, res)=> {
-    res.render('home', {url: req.protocol + "://" + req.headers.host});
+    res.render('home', {reqURL: req.protocol + "://" + req.headers.host});
 });
 
 route.get('/books', (req, res)=> {
-    res.render('books', {url: req.protocol + "://" + req.headers.host,
+    res.render('books', {reqURL: req.protocol + "://" + req.headers.host,
                         title: "Pride and Prejudice",
                         description: "classic love story"});
 });
 
 route.get('/authors', (req, res)=> {
-    res.render('authors', {url: req.protocol + "://" + req.headers.host,
+    res.render('authors', {reqURL: req.protocol + "://" + req.headers.host,
                            name: "Rabindranath Tagore"});
 });
 
