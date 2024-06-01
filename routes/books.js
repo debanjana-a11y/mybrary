a641472a4df0a3d3fba4595bac84cfadaaa01337
debanjana-a11y <89571066+debanjana-a11y@users.noreply.github.com>
2024-06-01 09:42:22 +0530

    Adds default book cover

--- routes/books.js
+++ routes/books.js
@@ -9,7 +9,7 @@ const books = [
         "Genre" : "Mystery",
         "PageCount" : 264,
         "Description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        "Image": "book1"
+        "Image": "book1.png"
     },
     {
         "Title" : "The Handmaid's Tale",
@@ -17,21 +17,24 @@ const books = [
         "Genre" : "Drama",
         "PageCount" : 311,
         "Description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        "Image": "book2"
+        "Image": "book2.png"
     },
     {
         "Title" : "The Raven King",
         "Author" : "Maggie Stiefvater",
         "Genre" : "Fantasy",
         "PageCount" : 439,
-        "Description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        "Image": "book3"
+        "Description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     }
 ];
 
 const addBookTodb = async (books) => {
     try {
         for (const book of books) {
+            if (book.hasOwnProperty("Image") == false) {
+                book["Image"] = 'default-cover.jpg';
+            }
+            console.log(book);
             bookToAdd = new booksDB(book);
             await bookToAdd.save();
         }
@@ -51,8 +54,9 @@ router.get('/addbook', async (req, res)=> {
     res.render('newbook', {reqURL: req.protocol + "://" + req.headers.host, books});
 });
 
-router.get('/:id', (req, res)=> {
-    res.render('show', {reqURL: req.protocol + "://" + req.headers.host, books, index: parseInt(req.params.id)});
+router.get('/:id', async (req, res)=> {
+    const bookList = await booksDB.find({});
+    res.render('show', {reqURL: req.protocol + "://" + req.headers.host, books: bookList, index: parseInt(req.params.id)});
 });
 
 module.exports = router;
