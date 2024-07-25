89914d0e516e158a8e8913f229a26c3c9f72422d
debanjana-a11y <89571066+debanjana-a11y@users.noreply.github.com>
2024-07-25 22:33:49 +0530

    Adds Author Info in Author page dynamically

--- routes/authors.js
+++ routes/authors.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const booksDB = require('../model/books');
 
-authorList = ['Agatha Christie', 'Margaret Atwood', 'Maggie Stiefvater'];
+authorList = [];
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+	const bookList = await booksDB.find({});
+	bookList.forEach(book => {
+		authorList.push(book.Author);
+	});
 	res.render('authors', {
 		reqURL: req.protocol + '://' + req.headers.host,
 		authors: authorList,
