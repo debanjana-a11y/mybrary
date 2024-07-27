0c39f2b90297037c25d990a67e87c768846f40fa
debanjana-a11y <89571066+debanjana-a11y@users.noreply.github.com>
2024-07-27 22:51:35 +0530

    check for production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./views/**/*.ejs"],
+	purge: [
+		// Your CSS will rebuild any time *any* file in `src` changes
+		'./public/**/*.{html,js}',
+	],
 	theme: {
 	  extend: {},
 	},
